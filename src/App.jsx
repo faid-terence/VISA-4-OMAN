813bd210102e7e7845fc7d7807bf8aa6662101c4
Terence Faid JABO <faidterence@outlook.com>
2024-02-19 16:32:26 +0200

    feature/visa-type-component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/Footer/Footer";
 import { ApplicationOptions } from "./components/ApplicationOptions/ApplicationOptions";
 import StatisticsComponent from "./components/Statics/StaticsComponent";
 import { Faq } from "./components/FAQ/Faq";
+import { VisaType } from "./components/VisaType/VisaType";
 
 function App() {
   const [count, setCount] = useState(0);
